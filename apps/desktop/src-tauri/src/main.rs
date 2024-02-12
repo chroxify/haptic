b0de0321f2b54552ba13951dc9eb8d2cca40c613
chroxify <chvtodorov@gmail.com>
2024-02-12 21:40:29 +0100

    fix: add tauri window state plugin to persist window size

--- apps/desktop/src-tauri/src/main.rs
+++ apps/desktop/src-tauri/src/main.rs
@@ -20,6 +20,7 @@ fn main() {
     })
     .invoke_handler(tauri::generate_handler![commands::folder::show_in_folder])
     .plugin(tauri_plugin_fs_watch::init())
+    .plugin(tauri_plugin_window_state::Builder::default().build())
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
