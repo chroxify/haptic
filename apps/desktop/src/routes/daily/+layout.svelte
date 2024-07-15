7e4193d468be7afffbb28016177441244e9f68ea
chroxify <chvtodorov@gmail.com>
2024-07-15 15:39:03 +0200

    feat: add details sidebar to daily notes too

--- apps/desktop/src/routes/daily/+layout.svelte
+++ apps/desktop/src/routes/daily/+layout.svelte
@@ -1,18 +1,34 @@
 <script lang="ts">
-	import DailySidebar from './sidebar.svelte';
-	import { isPageSidebarOpen, pageSidebarWidth, resizingPageSidebar } from '@/store';
+	import NotesSidebar from './sidebar.svelte';
+	import NoteDetails from './details.svelte';
+	import {
+		isPageSidebarOpen,
+		pageSidebarWidth,
+		resizingPageSidebar,
+		isNoteDetailSidebarOpen,
+		noteDetailSidebarWidth,
+		resizingNoteDetailSidebar
+	} from '@/store';
 </script>
 
+<!-- TODO: Think of refactoring layout & note details into shared component as /notes & /daily use the same code -->
 <div
 	class="flex flex-col w-full h-[calc(100vh-4.5rem)] bg-secondary-background ml-12 overflow-hidden"
 >
-	<DailySidebar />
+	<NotesSidebar />
 	<div
 		class="h-full overflow-y-auto"
-		style={`margin-left: ${$isPageSidebarOpen ? $pageSidebarWidth : 0}px; transition: ${
-			$resizingPageSidebar ? 'none' : 'margin-left 300ms'
-		}`}
+		style={`
+			margin-left: ${$isPageSidebarOpen ? $pageSidebarWidth : 0}px; 
+			margin-right: ${$isNoteDetailSidebarOpen ? $noteDetailSidebarWidth : 0}px; 
+			transition: ${
+				$resizingPageSidebar || $resizingNoteDetailSidebar
+					? 'none'
+					: 'margin-left 300ms, margin-right 300ms'
+			}
+		`}
 	>
 		<slot />
 	</div>
+	<NoteDetails />
 </div>
