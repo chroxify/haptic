e9b1db0107db6ba2b0a059ce9f2476c965a40e01
Christo Todorov <chroxify@gmail.com>
2024-08-18 17:36:45 +0200

    feat(homepage): add main page

---
+++ apps/homepage/src/lib/redirects.ts
@@ -0,0 +1,12 @@
+// Define the type for the redirects object
+type Redirects = {
+	[key: string]: string;
+};
+
+export const redirects: Redirects = {
+	'/app': 'https://app.haptic.md',
+	'/github': 'https://go.haptic.md/github',
+	'/twitter': 'https://go.haptic.md/twitter',
+	'/sponsor': 'https://go.haptic.md/sponsor',
+	'/tweet': 'https://go.haptic.md/tweet'
+};
