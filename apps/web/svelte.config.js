60612955f208bae75507d99e8a1ef62b7f64e451
Christo Todorov <chroxify@gmail.com>
2024-09-03 20:34:12 +0200

    fix(web): change adapter & dockerfile

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
